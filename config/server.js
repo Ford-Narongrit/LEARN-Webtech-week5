3dc9f5d587842ecda39b73c063050135acc23de9
ford-narongrit <narongrit.th@ku.th>
2021-07-30 21:00:59 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2a2706a3f1463bb346898a4ba7cbe68a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'edfee1d4-7d85-47f6-bfe7-e48ea627e513'
+};
